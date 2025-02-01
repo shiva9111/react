1fc42d1b77ab4093980a925417ac2461822b608e
shiva9111 <rshivamanish@gmail.com>
2025-02-01 11:41:41 +0530

    added

---
+++ src/App.js
@@ -0,0 +1,32 @@
+// // import logo from './logo.svg';
+// import './App.css';
+// import React from "react";
+
+// function App() {
+//   let username = "Shiva";
+//   return (
+//     <div>
+//       <h1>company</h1>
+//       <p>para</p>
+//       {username}
+//       {100*100}
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+import React from "react";
+
+class App extends React.Component{
+  render(){
+    return(
+      <div>
+        <h1>Class Component</h1>
+      </div>
+    )
+  }
+}
+
+export default App;
