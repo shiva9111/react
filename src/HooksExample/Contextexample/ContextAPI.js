8d1cb4799a779697d245a83da6173ebb1b9c24c4
shiva9111 <rshivamanish@gmail.com>
2025-02-12 11:54:20 +0530

    add

--- src/HooksExample/Contextexample/ContextAPI.js
+++ src/HooksExample/Contextexample/ContextAPI.js
@@ -1,9 +1,12 @@
-import React, { createContext } from 'react'
-const GlobalDataProvider = createContext();
-const ContextAPI = (props) => {
-  return (
-    <div>ContextAPI</div>
-  )
-}
+// import React, { createContext, useState } from 'react'
+// const GlobalDataProvider = createContext();
+// const ContextAPI = (props) => {
+//     const[count , setCount] = useState();
+//   return <GlobalDataProvider.Provider value="Sam">
+//     {props.children}
+//   </GlobalDataProvider.Provider>(
+//     <div>ContextAPI</div>
+//   )
+// }
 
-export default ContextAPI
+// export default ContextAPI
